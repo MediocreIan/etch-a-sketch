fd4876db16a694c362e6e690eef29e1c31e11d15
Ian Drews <idrews094@gmail.com>
2018-03-26 16:57:04 -0500

    added comments explaining javascript file

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let userInput = document.getElementById('userInput').value
-let makeCell = function(sizeInput){
+let makeCell = function(sizeInput){   //currently handles entire javascript function needs to be split up
 	for (var i = 0; i < sizeInput * sizeInput; i++) {
-		document.getElementById('wrapper');
+		document.getElementById('wrapper');  //identifies parent div. div is mad flexbox by css rules
 		let cellHeight =(1 / sizeInput * 600) - 2 + 'px';
-		let cellWidth = (1 / sizeInput * 700) - 2 + 'px';
-		let cell = document.createElement('div');
-		cell.style.width = cellWidth;
+		let cellWidth = (1 / sizeInput * 700) - 2 + 'px'; // makes the size of cells proportional to wrapper size - margin
+		let cell = document.createElement('div');  //the information for creating individual cells, sould be made its own function
+		cell.style.width = cellWidth;  
 		cell.style.height = cellHeight;
 		cell.style.margin = '1px'
 		cell.style.backgroundColor = 'black' 
@@ -13,3 +13,4 @@ let makeCell = function(sizeInput){
 		
 	}
 }
+ 
